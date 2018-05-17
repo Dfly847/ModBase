014c94ad0328bc9d4f4a00c07830b0d4493688fc
Dfly847 <Dfly847@users.noreply.github.com>
2018-05-16 19:30:38 -0700

    Added Tables in other wood types

--- java/com/dfly847/modbase/init/ModBlocks.java
+++ java/com/dfly847/modbase/init/ModBlocks.java
@@ -11,7 +11,11 @@ import net.minecraft.block.material.Material;
 public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	
-	public static final Block TABLE_OAK = new TableBlock("table_oak", Material.WOOD);
 
+	public static final Block TABLE_OAK = new TableBlock("table_oak", Material.WOOD);
+	public static final Block TABLE_SPRUCE = new TableBlock("table_spruce", Material.WOOD);
+	public static final Block TABLE_BIRCH = new TableBlock("table_birch", Material.WOOD);
+	public static final Block TABLE_JUNGLE = new TableBlock("table_jungle", Material.WOOD);
+	public static final Block TABLE_ACACIA = new TableBlock("table_acacia", Material.WOOD);
+	public static final Block TABLE_DARKOAK = new TableBlock("table_darkoak", Material.WOOD);
 }
